2eda119a5236729d54e11b1a8275bc503d1fe5db
linyangz <linyangz@outlook.com>
2023-08-02 06:27:17 +0000

    微调首页样式

--- client/web/src/app/page.tsx
+++ client/web/src/app/page.tsx
@@ -1,17 +1,20 @@
 import { loadHeaderNav } from '@/components/nav'
 import React from 'react'
-import styles from './page.module.css'
+import styles from './page.module.scss' 
 
 export default async function Home () {
   const headerNav = await loadHeaderNav()
-  return <div>
-
-<header>
+  return <div className={styles.indexPage}>
+        <div className={styles.navRow}>
           {headerNav}
-        </header>
-        <main className={styles.layoutChildren}>
-          统一的门户站点，认证中心
-        </main>
-        <footer></footer>
+        </div>
+        <div className={styles.layoutChildren}>
+{/*           
+          <Image className={styles.mainImage} src="/images/background/bear.webp" alt="banner"
+            height={600} width={600}/>
+            */}
+           首页
+
+        </div>
   </div>
 }
