e1e1706a8d3b3571a8eed25367984e460cb8eead
linyangz <linyangz@outlook.com>
2023-09-17 10:29:45 +0000

    修改默认redis db为0

--- client/web/config.debug.js
+++ client/web/config.debug.js
@@ -5,7 +5,7 @@ const serverConfig = {
   RESOURCE_SERVER: 'https://fields.huable.xyz', 
   AES_KEY: 'vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3',
   AES_IV: 'cwVPn3yX3sp6Bbj0', 
-  REDIS: 'redis://127.0.0.1:6379/4',
+  REDIS: 'redis://127.0.0.1:6379/0',
   REDIS_PASSWORD: 'AS7xQsqVzbHMMK5',
   AUTH_SERVER: 'https://teleport.huable.xyz',
 }
--- client/web/config.js
+++ client/web/config.js
@@ -5,7 +5,7 @@ const serverConfig = {
   RESOURCE_SERVER: 'https://fields.huable.com',
   AES_KEY: 'vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3',
   AES_IV: 'cwVPn3yX3sp6Bbj0',
-  REDIS: 'redis://10.0.0.4:6379/3',
+  REDIS: 'redis://10.0.0.4:6379/0',
   REDIS_PASSWORD: '^P^c&mSVoNgU08o6',
   AUTH_SERVER: 'https://teleport.huable.com',
 }
