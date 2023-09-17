f49b72f9a437ed878061eb761b3a7b7281cbe364
linyangz <linyangz@outlook.com>
2023-09-17 09:14:55 +0000

    修改Redis配置

--- client/web/config.js
+++ client/web/config.js
@@ -5,8 +5,8 @@ const serverConfig = {
   RESOURCE_SERVER: 'https://fields.huable.com',
   AES_KEY: 'vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3',
   AES_IV: 'cwVPn3yX3sp6Bbj0',
-  REDIS: 'redis://127.0.0.1:6379/3',
-  REDIS_PASSWORD: 'AS7xQsqVzbHMMK5',
+  REDIS: 'redis://10.0.0.4:6379/3',
+  REDIS_PASSWORD: '^P^c&mSVoNgU08o6',
   AUTH_SERVER: 'https://teleport.huable.com',
 }
   
