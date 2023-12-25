1c26d203d5d6f2d7d08c5a96291db074e85d7cea
linyangz <linyangz@outlook.com>
2023-12-25 08:00:22 +0000

    更新了下flutter包的版本 增加multiverse workspace配置

--- quantum/flutter/quantum-flutter-sqlite/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ quantum/flutter/quantum-flutter-sqlite/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import path_provider_foundation
-import quantum_sqlite
 import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  QuantumPlugin.register(with: registry.registrar(forPlugin: "QuantumPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
 }
