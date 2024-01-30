c2f6f29f1a1ef578bb58af35ddb1831173edc71b
linyangz <linyangz@outlook.com>
2024-01-30 06:50:54 +0000

    尝试为服务器和客户端平台导出不同的文件

--- stele/rollup.config.mjs
+++ stele/rollup.config.mjs
@@ -4,19 +4,25 @@ import { visualizer } from 'rollup-plugin-visualizer'
 import strip from '@rollup/plugin-strip'
 import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
+import pkg from './package.json' with {type: 'json'};
 import postcss from 'rollup-plugin-postcss'
+import scss from 'rollup-plugin-scss'
 import del from 'rollup-plugin-delete'
 import json from '@rollup/plugin-json'
+import terser from '@rollup/plugin-terser';
+import autoprefixer from 'autoprefixer'
 
-export default [{
-  input: 'src/index.tsx',
+const commonConfig = [{
+  input: 'src/index.common.tsx',
   output: {
-    file: 'lib/index.mjs',
+    file: 'lib/index.common.mjs',
     format: 'esm',
     sourcemap: true,
     assetFileNames: '[name][extname]'
   },
-  external: ['react', 'util', 'buffer', 'events', 'os', 'fs', 'path', 'http', 'stream', 'https', 'zlib', 'tty'],
+  external: [
+    ...Object.keys(pkg.dependencies || {})
+  ],
   plugins: [
     del({ targets: 'lib/*' }),
     commonjs(),
@@ -27,28 +33,208 @@ export default [{
     }),
     typescript(),
     json(),
+    scss({ fileName: 'index.css' }),
     postcss({
-      modules: true
+      plugins: [autoprefixer()],
+      sourceMap: true,
+      extract: true,
+      minimize: true,
+      writeDefinitions: true,
+      modules: true,
+      namedExports: true,
     }),
-    //scss({fileName: 'bundle.css'}),
     strip({
       include: ['**/*.(js|mjs|ts|tsx)'],
       debugger: true,
       functions: ['console.log', 'console.debug'],
       sourceMap: true
     }),
-    
+    terser(),
     visualizer({
-      filename: 'lib/status.html'
+      filename: 'build/status.common.html'
     })
   ]
+},{
+  input: 'src/index.common.tsx',
+  output: {
+    file: 'lib/index.common.cjs',
+    format: 'cjs',
+    sourcemap: true,
+    assetFileNames: '[name][extname]'
+  },
+  external: [
+    ...Object.keys(pkg.dependencies || {})
+  ],
+  plugins: [
+    del({ targets: 'lib/*' }),
+    commonjs(),
+    nodeResolve({
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      moduleDirectories: ['node_modules', 'src'],
+      preferBuiltins: false
+    }),
+    typescript(),
+    json(),
+    strip({
+      include: ['**/*.(js|mjs|ts|tsx)'],
+      debugger: true,
+      functions: ['console.log', 'console.debug'],
+      sourceMap: true
+    }),
+    terser(),
+    visualizer({
+      filename: 'build/status.common.html'
+    })
+  ]
+},
+{
+  input: 'lib/dts/index.common.d.ts',
+  output: [{ file: 'lib/index.common.d.ts' }],
+  external: [/\.(css|scss)$/],
+  plugins: [
+    dts()
+  ]
 }
-// {
-//   input: 'lib/dts/index.d.ts',
-//   output: [{ file: 'lib/index.d.ts' }],
-//   external: [/\.(css|scss)$/],
-//   plugins: [
-//     dts()
-//   ]
-// }
 ]
+
+const serverConfig = [{
+  input: 'src/index.server.tsx',
+  output: {
+    file: 'lib/index.server.mjs',
+    format: 'esm',
+    sourcemap: true,
+    assetFileNames: '[name][extname]'
+  },
+  external: ['react'],
+  plugins: [
+    commonjs(),
+    nodeResolve({
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      moduleDirectories: ['node_modules', 'src'],
+      preferBuiltins: false
+    }),
+    typescript(),
+    json(),
+    strip({
+      include: ['**/*.(js|mjs|ts|tsx)'],
+      debugger: true,
+      functions: ['console.log', 'console.debug'],
+      sourceMap: true
+    }),
+    terser(),
+    visualizer({
+      filename: 'build/status.server.html'
+    })
+  ]
+},{
+  input: 'src/index.server.tsx',
+  output: {
+    file: 'lib/index.server.cjs',
+    format: 'cjs',
+    sourcemap: true,
+    assetFileNames: '[name][extname]'
+  },
+  external: ['react'],
+  plugins: [
+    commonjs(),
+    nodeResolve({
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      moduleDirectories: ['node_modules', 'src'],
+      preferBuiltins: false
+    }),
+    typescript(),
+    json(),
+    strip({
+      include: ['**/*.(js|mjs|ts|tsx)'],
+      debugger: true,
+      functions: ['console.log', 'console.debug'],
+      sourceMap: true
+    }),
+    terser(),
+    visualizer({
+      filename: 'build/status.server.html'
+    })
+  ]
+},
+{
+  input: 'lib/dts/index.server.d.ts',
+  output: [{ file: 'lib/index.server.d.ts' }],
+  external: [/\.(css|scss)$/],
+  plugins: [
+    dts()
+  ]
+}
+]
+
+const clientConfig = [{
+  input: 'src/index.client.tsx',
+  output: {
+    file: 'lib/index.client.mjs',
+    format: 'esm',
+    sourcemap: true,
+    assetFileNames: '[name][extname]'
+  },
+  external: ['react'],
+  plugins: [
+    commonjs(),
+    nodeResolve({
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      moduleDirectories: ['node_modules', 'src'],
+      preferBuiltins: false
+    }),
+    typescript(),
+    json(),
+    strip({
+      include: ['**/*.(js|mjs|ts|tsx)'],
+      debugger: true,
+      functions: ['console.log', 'console.debug'],
+      sourceMap: true
+    }),
+    terser(),
+    visualizer({
+      filename: 'build/status.client.html'
+    })
+  ]
+},{
+  input: 'src/index.client.tsx',
+  output: {
+    file: 'lib/index.client.cjs',
+    format: 'cjs',
+    sourcemap: true,
+    assetFileNames: '[name][extname]'
+  },
+  external: ['react'],
+  plugins: [
+    commonjs(),
+    nodeResolve({
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      moduleDirectories: ['node_modules', 'src'],
+      preferBuiltins: false
+    }),
+    typescript(),
+    json(),
+    strip({
+      include: ['**/*.(js|mjs|ts|tsx)'],
+      debugger: true,
+      functions: ['console.log', 'console.debug'],
+      sourceMap: true
+    }),
+    terser(),
+    visualizer({
+      filename: 'build/status.client.html'
+    })
+  ]
+},
+{
+  input: 'lib/dts/index.client.d.ts',
+  output: [{ file: 'lib/index.client.d.ts' }],
+  external: [/\.(css|scss)$/],
+  plugins: [
+    dts()
+  ]
+}
+]
+
+const exportConfig = commonConfig.concat(serverConfig).concat(clientConfig)
+
+export default exportConfig
--- stele/src/utils/string.ts
+++ stele/src/utils/string.ts
@@ -5,7 +5,7 @@ export function generatorRandomString (length: number) {
   return result
 }
 
-export function subString (str: string | undefined, length: number,
+export function STSubString (str: string | undefined, length: number,
   suffix: string = '...') {
   if (!str || length <= 0) return ''
   return str.length > 100 ? str.slice(0, length) + suffix : str
