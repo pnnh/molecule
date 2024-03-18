7f89e748840c013db2db4d0048b15d3c149604a4
linyangz <linyangz@outlook.com>
2024-03-18 12:11:21 +0000

    构建时生成typescript类型定义

--- pulsar/README.md
+++ pulsar/README.md
@@ -9,3 +9,11 @@ cmake --build --preset macOS --config Release
 # 安装native库到指定目录
 cmake --install build/macOS --config Release
 ```
+
+### 生成Web库
+
+似乎需要将emscripten工具链升级到3.1.55版本以上，否则无法生成typescript定义并且会编译报错
+```bash
+cmake --preset Web
+cmake --build --preset Web --verbose
+```
--- pulsar/web/wasm.cpp
+++ pulsar/web/wasm.cpp
@@ -1,8 +1,20 @@
-#include <emscripten/bind.h>
+
 #include "wasm.h"
+
+#include <emscripten/bind.h>
+#include <iostream>
 
 using namespace emscripten;
 
 float lerp(float a, float b, float t) {
     return (1 - t) * a + t * b;
+}
+
+EMSCRIPTEN_BINDINGS(module) {
+    function("lerp", &lerp);
+}
+
+int main(int argc, char *argv[]) {
+    auto abc = lerp(2.4, 4.3, 0.5);
+    std::cout << "lerp(2.4, 4.3, 0.5) = " << abc << std::endl;
 }
