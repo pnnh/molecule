88b4ef46a0222f6e24bec00afaf5c6e45e23ea0d
linyangz <linyangz@outlook.com>
2024-03-19 10:34:57 +0000

    调整pulsar代码，重复代码合并或移除

--- pulsar/server/business/src/controllers/index.cpp
+++ pulsar/server/business/src/controllers/index.cpp
@@ -12,5 +12,21 @@ void HandleIndex(boost::beast::http::response<boost::beast::http::dynamic_body> 
     response_.keep_alive(false);
     response_.set(boost::beast::http::field::server, "Beast");
 
-    boost::beast::ostream(response_.body()) << "Hello World";
+    //boost::beast::ostream(response_.body()) << "Hello World";
+
+
+    //response_.set(boost::beast::http::field::content_type, "text/html");
+    std::ifstream infile;
+    infile.open("assets/index.html");
+
+    std::string path = "assets/index.html";
+
+    std::string full_path = "assets/index.html";
+
+    response_.result(boost::beast::http::status::ok);
+    response_.keep_alive(false);
+    response_.set(boost::beast::http::field::server, "Beast");
+    response_.set(boost::beast::http::field::content_type, mime_type(std::string(full_path)));
+
+    boost::beast::ostream(response_.body()) << infile.rdbuf();
 }
--- pulsar/server/business/src/server/http_connection.h
+++ pulsar/server/business/src/server/http_connection.h
@@ -13,6 +13,7 @@
 #include <ctime>
 #include <iostream>
 #include <memory>
+#include "state.h"
 
 class http_connection : public std::enable_shared_from_this<http_connection> {
 public:
@@ -20,21 +21,39 @@ public:
             : socket_(std::move(socket)) {
     }
 
+    // Initiate the asynchronous operations associated with the connection.
+    void start() {
+        read_request();
+        check_deadline();
+    }
+
+private:
     boost::asio::ip::tcp::socket socket_;
-    
+
     boost::beast::flat_buffer buffer_{8192};
 
     boost::beast::http::request<boost::beast::http::dynamic_body> request_;
 
     boost::beast::http::response<boost::beast::http::dynamic_body> response_;
-
-    boost::optional<boost::beast::http::response<boost::beast::http::file_body>> file_response_;
-
-    boost::optional<boost::beast::http::response_serializer<boost::beast::http::file_body>> file_serializer_;
+//
+//    boost::optional<boost::beast::http::response<boost::beast::http::file_body>> file_response_;
+//
+//    boost::optional<boost::beast::http::response_serializer<boost::beast::http::file_body>> file_serializer_;
 
     boost::asio::steady_timer deadline_{
             socket_.get_executor(), std::chrono::seconds(60)};
 
+    void read_request();
+
+    void process_request();
+
+    void create_response();
+
+    void write_response();
+
+    void send_file();
+
+    void check_deadline();
 };
 
 #endif //CPP_SERVER_HTTP_CONNECTION_H
