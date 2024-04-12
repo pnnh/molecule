b007aa33764a781a9785e2bd9d72f7d30cafc228
develop-windows\azure <linyangz@outlook.com>
2024-04-12 06:03:33 +0000

    尝试调整windows下打印日志，未完成

--- desktop/main.cpp
+++ desktop/main.cpp
@@ -1,44 +1,55 @@
-#include "services/SqliteService.h"
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QQmlContext>
+// #include "services/SqliteService.h"
+// #include <QGuiApplication>
+// #include <QQmlApplicationEngine>
+// #include <QQmlContext>
+#include <QDebug>
+#include <QLoggingCategory>
 #include <QQmlDebuggingEnabler>
 #include <QQuickWindow>
 #include <iostream>
 
-#if TARGET_OS_MAC
-  #include "platform/macos/objc_code.h"
-#endif
-#include "services/SqliteService.h"
+// #if TARGET_OS_MAC
+//   #include "platform/macos/objc_code.h"
+// #endif
+// #include "services/SqliteService.h"
 
-int main(int argc, char *argv[]) {
-  QQmlDebuggingEnabler::enableDebugging(true);
-
-  QGuiApplication app(argc, argv);
-  app.setApplicationDisplayName(
-      QStringLiteral("This example is powered by qmltc!"));
-
-  QQmlApplicationEngine engine;
-  const QUrl url(QStringLiteral(u"qrc:/qt/qml/quick/src/module/views/Main.qml"));
-
-  // QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
-  //     &app, []() { QCoreApplication::exit(-1); },
-  //     Qt::QueuedConnection);
-
-  engine.load(url);
-
-  auto dbName = QGuiApplication::applicationDirPath() + "/venus22.sqlite";
-
-  auto svc = std::make_shared<services::Sqlite3Service>(dbName);
-  std::cout << "macos call: " << svc -> sqlite3Version().toStdString() << std::endl;
-
-  return app.exec();
+void sayHello()
+{
+  std::cout << "Hello, World!" << std::endl;
+  // #if TARGET_OS_MAC
+  //   std::cout << "macos call: " << localizedHostName().toStdString() << std::endl;
+  // #endif
 }
 
+int main(int argc, char *argv[])
+{
+  QQmlDebuggingEnabler::enableDebugging(true);
+  QLoggingCategory::defaultCategory()->setEnabled(QtDebugMsg, true);
+  qInfo() << "test info" << std::endl;
+  qWarning() << "test warning" << std::endl;
+  std::cerr << "Hello, World333333!" << std::endl;
+  sayHello();
+  qDebug() << "Hello, World444444!";
+  getchar();
+  return 0;
 
-void sayHello() {
-  std::cout << "Hello, World!" << std::endl;
-  #if TARGET_OS_MAC
-    std::cout << "macos call: " << localizedHostName().toStdString() << std::endl;
-  #endif
+  // QGuiApplication app(argc, argv);
+  // app.setApplicationDisplayName(
+  //     QStringLiteral("This example is powered by qmltc!"));
+
+  // QQmlApplicationEngine engine;
+  // const QUrl url(QStringLiteral(u"qrc:/qt/qml/quick/src/module/views/Main.qml"));
+
+  // // QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
+  // //     &app, []() { QCoreApplication::exit(-1); },
+  // //     Qt::QueuedConnection);
+
+  // engine.load(url);
+
+  // auto dbName = QGuiApplication::applicationDirPath() + "/venus22.sqlite";
+
+  // auto svc = std::make_shared<services::Sqlite3Service>(dbName);
+  // std::cout << "macos call: " << svc -> sqlite3Version().toStdString() << std::endl;
+
+  // return app.exec();
 }
