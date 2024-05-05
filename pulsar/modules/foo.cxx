10dad56005599ee5d96a2e2cbb74d4beb36367c9
linyangz <linyangz@outlook.com>
2024-05-05 12:39:43 +0000

    在clang编译器下配置clang-tidy并编译通过 配置vscode下clang-tidy检验格式

--- pulsar/modules/foo.cxx
+++ pulsar/modules/foo.cxx
@@ -8,10 +8,8 @@ export class foo
 {
 public:
     foo();
-    ~foo();
     void helloworld();
 };
 
 foo::foo() = default;
-foo::~foo() = default;
 void foo::helloworld() { std::cout << "hello world\n"; }
