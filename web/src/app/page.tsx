5ef2bdacbbaf7d23c4d63cae794f6cd9168077ea
larry <xspanni@gmail.com>
2024-05-10 14:35:30 +0800

    修复首页获取URL报错

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -3,9 +3,8 @@ import styles from './page.module.scss'
 import { getIdentity } from '@/services/auth'
 import { PublicNavbar } from './partials/navbar'
 import Image from 'next/image'
-
 import { lerpServer } from '@/services/server/wasm-server'
-import {headers} from "~/next/headers";
+import {getPathname} from "@/services/server/pathname";
 
 export default async function Home({ searchParams }: {
     searchParams: Record<string, string>
@@ -14,13 +13,10 @@ export default async function Home({ searchParams }: {
 
     const a = lerpServer(1, 2, 0.5)
     console.debug('axxxx server:', a)
-    const headersList = headers();
-    const fullUrl = headersList.get('referer') || "";
-    const url = new URL(fullUrl)
     const identity = await getIdentity()
     return <div className={styles.fullPage}>
         <div>
-            <PublicNavbar account={identity} pathname={url.pathname} />
+            <PublicNavbar account={identity} pathname={getPathname()} />
         </div>
         <div className={styles.mainContainer}>
             <div className={styles.contentCenter}>
