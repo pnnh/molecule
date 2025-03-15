39350c64e3fff27ce20707f1bb11a504bca4b3ef
linya <xspanni@gmail.com>
2025-03-15 15:19:47 +0800

    移除无关文件

--- Molecule/Models/CommonResult.cs
+++ Molecule/Models/CommonResult.cs
@@ -1,46 +1,79 @@
-
 using System.Text.Json.Serialization;
 
-namespace Molecule.Models;
+namespace Polaris.Business.Models;
 
-public class CommonResult<T>
+public class PLBizException : Exception
 {
-    public Codes Code { get; init; } = Codes.Ok;
-    public string? Message { get; init; } = "";
-    public T? Data { get; init; } = default(T);
-}
-
-public class SelectData<T>
-{
-    public int Count { get; init; } = 0;
-    public List<T> List { get; init; } = new List<T>();
-}
-
-
-public class MSelectResult<T>
-{
-    [JsonPropertyName("page")] public int Page { get; set; }
-
-    [JsonPropertyName("size")] public int Size { get; set; }
-
-    [JsonPropertyName("count")] public int Count { get; init; }
-
-    [JsonPropertyName("range")] public List<T> Range { get; init; } = new();
-
-    public static MSelectResult<T> New(int page, int size, int count, List<T> range)
+    public PLBizException(string message, int code = (int)PLCodes.Error) : base(message)
     {
-        return new MSelectResult<T>
-        {
-            Page = page,
-            Size = size,
-            Count = count,
-            Range = range
-        };
+        Code = code;
     }
 
-    public MSelectResult<T> AddRange(params T[] range)
+    public PLBizException(int code = (int)PLCodes.Error, string publicMessage = "",
+        string internalMessage = "") : base(publicMessage + "\n" + internalMessage)
     {
-        Range.AddRange(range);
-        return this;
+        Code = code;
+        InternalMessage = internalMessage;
+        PublicMessage = publicMessage;
+    }
+
+    public int Code { get; set; }
+    public string InternalMessage { get; set; } = "";
+    public string PublicMessage { get; set; } = "";
+
+    public static PLBizException BadRequest(string message)
+    {
+        return new PLBizException(message, (int)PLCodes.BadRequest);
+    }
+
+    public static PLBizException NotFound(string message)
+    {
+        return new PLBizException(message, (int)PLCodes.NotFound);
+    }
+
+    public static PLBizException Error(string message)
+    {
+        return new PLBizException(message);
+    }
+
+    public static PLBizException Unauthorized(string message)
+    {
+        return new PLBizException(message, (int)PLCodes.Unauthorized);
+    }
+
+    public static PLBizException New(int code = (int)PLCodes.Error, string publicMessage = "",
+        string internalMessage = "")
+    {
+        return new PLBizException(code, publicMessage, internalMessage);
+    }
+
+    public static PLBizException New(PLCodes code = PLCodes.Error, string publicMessage = "",
+        string internalMessage = "")
+    {
+        return new PLBizException((int)code, publicMessage, internalMessage);
     }
 }
+
+public enum PLCodes
+{
+    Ok = 200,
+    BadRequest = 400,
+    Unauthorized = 401,
+    NotFound = 404,
+    Error = 500,
+    Pending = 600, // 业务处理中
+    InvalidArgument = 601
+}
+
+public class PLExceptionResult
+{
+    [JsonPropertyName("code")] public int Code { get; set; }
+
+    [JsonPropertyName("message")] public string Message { get; set; } = "";
+}
+
+public class PModifyResult
+{
+    public Guid Uid { get; set; }
+    public int Changes { get; set; }
+} 
